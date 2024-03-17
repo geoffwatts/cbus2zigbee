9b57bc28a10107f61d9ccc1eea7e08827058f898
Geoff Watts <wattsg@gmail.com>
2024-03-18 00:20:40 +0800

    clarified comments

--- zigbee resident.lua
+++ zigbee resident.lua
@@ -9,7 +9,7 @@ Update your MQTT credentials below:
 mqtt_broker = '192.168.1.1'
 mqtt_username = ''
 mqtt_password = ''
-mqtt_clientid = 'cbuszigbee'
+mqtt_clientid = 'cbus2zigbee'
 
 zPort = 0xBEEF1
 
@@ -23,19 +23,21 @@ mqtt = require("mosquitto")
 -- create new mqtt client
 client = mqtt.new(mqtt_clientid)
 
-client:will_set('cbus2zigbee/status', 'offline', 2, true)
+client:will_set(mqtt_clientid..'/status', 'offline', 2, true)
 client:login_set(mqtt_username, mqtt_password)
 
 client.ON_CONNECT = function()
   log("MQTT connected - ready to send commands to zigbee2mqtt")
-  client:publish('cbus2zigbee/status', 'online', 2, true)
+  client:publish(mqtt_clientid..'/status', 'online', 2, true)
 end
 
 client.ON_DISCONNECT = function()
   log("MQTT disconnected - attempting recovery and client reconnect")
+  -- Handled automatically by loop_start()
 end
 
 client:connect(mqtt_broker)
+-- I understand with loop_start, this will automatically re-connect to mqtt
 client:loop_start()
 
 -- C-Bus events to MQTT local listener
@@ -43,7 +45,7 @@ server = require('socket').udp()
 server:settimeout(1)
 server:setsockname('127.0.0.1', zPort)
 
--- Main loop: Connect to MQTT broker and process commands
+-- Main loop: process commands
 while true do
 
   -- Receive commands from C-Bus and publish to MQTT
@@ -58,5 +60,4 @@ while true do
             '","brightness":"'..level..'"}', 2, true) -- QoS 2 for send exactly once
     end
 
-    ::continue_loop:: -- Continue loop label
 end
